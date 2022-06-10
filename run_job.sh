20415ad6f4ffc0e5a100b3542433d5a9ae0c6a33
ibordeu <ibordeu@gmail.com>
2022-06-10 15:18:40 +0100

    up

--- run_job.sh
+++ run_job.sh
@@ -3,15 +3,15 @@
 OUTPUTDIR="BARW/"
 # Change parameters below and then run with chmod +x run_job.sh && ./run_job.sh
 # sample: ./bws -L 257 -N 1000 -h 0.2 -p 0.25 -q 0.25 > BARW/data_h_0.4_p_0.25_q_0.25.txt 
-SEED=1000 # Starting seed
-L=256  # system size 2**n+1
+SEED=1300 # Starting seed
+L=513  # system size 2**n+1
 N=1000 # number of realisations
-hmin=0.3 # branching to hopping rate ratio
-hmax=0.7 
+hmin=0.4 # branching to hopping rate ratio
+hmax=0.8 
 hstep=0.1 
 
-p=0.34 # prob. of hopping in the same direction as the previous hop 
-q=0.33 # prob. of hopping to one of the two orthogonal directions.
+p=0.25 # prob. of hopping in the same direction as the previous hop 
+q=0.25 # prob. of hopping to one of the two orthogonal directions.
 
 # ====================
 # Below this line, it's not necessary to change things
